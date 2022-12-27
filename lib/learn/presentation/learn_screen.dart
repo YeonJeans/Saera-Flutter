a97a1ab666e3fcc04de3fa64c3702ef8ca341269
whaeundo <1916388@sookmyung.ac.kr>
2022-12-27 15:12:47 +0900

    [Feat/#1] Create Splash Screen

---
+++ lib/learn/presentation/learn_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class LearnPaage extends StatefulWidget {
+  const LearnPaage({Key? key}) : super(key: key);
+
+  @override
+  State<LearnPaage> createState() => _LearnPaageState();
+}
+
+class _LearnPaageState extends State<LearnPaage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
