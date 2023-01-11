1325466b49dc6665cbf7a52dc86e78bdd6f0dbef
yeoncheong <yjin1100@sookmyung.ac.kr>
2023-01-11 22:13:59 +0900

    [Setting/#2] Add Color

--- lib/style/color.dart
+++ lib/style/color.dart
@@ -9,12 +9,15 @@ class ColorStyles {
   static const Color saeraWhite=Color(0xffFFFFFF);
 
   // Gray Color
-  static const Color searchGray = Color(0xffAAAAAA);
+  static const Color searchFillGray = Color(0xffEFEFEF);
+  static const Color searchTextGray = Color(0xffAAAAAA);
   static const Color dateExpGray = Color(0xff999999);
   static const Color recentInfoGray = Color(0xff828282);
   static const Color digitGray = Color(0xff666666);
   static const Color totalGray = Color(0xff555555);
 
   static const Color textBlack = Color(0xff333333);
+
+  static const Color etcYellow = Color(0xffE5DDD2);
 
 }
