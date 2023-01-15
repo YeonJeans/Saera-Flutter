36b0728f1c2e0c740397b165a25ddefe3284e040
yeoncheong <yjin1100@sookmyung.ac.kr>
2023-01-15 21:54:53 +0900

    [Feature/#5] Add home screen route

--- lib/home/presentation/bookmark_screen.dart
+++ lib/home/presentation/bookmark_screen.dart
@@ -13,48 +13,48 @@ class _BookmarkPageState extends State<BookmarkPage> {
   @override
   Widget build(BuildContext context) {
     Widget appBarSection = Container(
-      padding: const EdgeInsets.symmetric(vertical: 10),
+      padding: const EdgeInsets.symmetric(vertical: 5),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          ElevatedButton(
-              onPressed: null,
-              child: Row(
-                children: [
-                  SvgPicture.asset(
-                    'assets/icons/back.svg',
-                    fit: BoxFit.scaleDown,
-                  ),
-                  const Text(
-                      '뒤로',
-                      style: TextStyle(color: ColorStyles.saeraBlue, fontSize: 18, fontFamily: "NotoSansKR", fontWeight: FontWeight.normal)
-                  )
-                ],
-              )
+          TextButton.icon(
+              onPressed: () => Navigator.pop(context),
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.transparent
+              ),
+              icon: SvgPicture.asset(
+                'assets/icons/back.svg',
+                fit: BoxFit.scaleDown,
+              ),
+              label: const Text(' 뒤로', style: TextStyle(color: ColorStyles.primary, fontSize: 18, fontFamily: "NotoSansKR", fontWeight: FontWeight.normal))
+
           )
         ],
       ),
     );
 
-    return SafeArea(
-        child: Scaffold(
-          backgroundColor: Colors.transparent,
-          resizeToAvoidBottomInset: false,
-          body: Container(
-            decoration: const BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage('assets/images/home_detail_bg.png'),
-                    fit: BoxFit.fill
-                )
+    return Container(
+      color: const Color(0xfff0f8ff),
+      child: SafeArea(
+          child: Scaffold(
+            backgroundColor: Colors.transparent,
+            resizeToAvoidBottomInset: false,
+            body: Container(
+              decoration: const BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage('assets/images/home_detail_bg.png'),
+                      fit: BoxFit.fill
+                  )
+              ),
+              child: ListView(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                children: <Widget>[
+                  appBarSection
+                ],
+              ),
             ),
-            child: ListView(
-              padding: const EdgeInsets.symmetric(horizontal: 21),
-              children: <Widget>[
-                appBarSection
-              ],
-            ),
-          ),
-        )
+          )
+      ),
     );
   }
 }
