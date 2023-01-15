0ea523452a1f22c8c5aa0aa17d6606c0ca758c0f
whaeundo <1916388@sookmyung.ac.kr>
2023-01-15 22:35:52 +0900

    [Setting/#4] Add color and font

--- lib/style/font.dart
+++ lib/style/font.dart
@@ -15,9 +15,11 @@ class TextStyles {
 
   static const regular00TextStyle = TextStyle(fontFamily: "NotoSansKR", fontSize: 14, fontWeight: FontWeight.normal, color: Color(0xff000000));
   static const regular25TextStyle = TextStyle(fontFamily: "NotoSansKR", fontSize: 14, fontWeight: FontWeight.normal, color: Color(0xff252525));
+  static const regular25BoldTextStyle = TextStyle(fontFamily: "NotoSansKR", fontSize: 14, fontWeight: FontWeight.w700, color: Color(0xff252525));
   static const regular82TextStyle = TextStyle(fontFamily: "NotoSansKR", fontSize: 14, fontWeight: FontWeight.normal, color: Color(0xff828282));
   static const regularBlueTextStyle = TextStyle(fontFamily: "NotoSansKR", fontSize: 14, fontWeight: FontWeight.normal, color: Color(0xff3E68A8));
-  static const regularHighlightBluetTextStyle = TextStyle(fontFamily: "NotoSansKR", fontSize: 14, fontWeight: FontWeight.normal, color: Color(0xff86C4CC));
+  static const regularHighlightBlueTextStyle = TextStyle(fontFamily: "NotoSansKR", fontSize: 14, fontWeight: FontWeight.normal, color: Color(0xff86C4CC));
+  static const regularHighlightBlueBoldTextStyle = TextStyle(fontFamily: "NotoSansKR", fontSize: 14, fontWeight: FontWeight.w700, color: Color(0xff86C4CC));
 
 
   static const selectedTagTextStyle = TextStyle(fontFamily: "NotoSansKR", fontSize: 15, fontWeight: FontWeight.normal, color: Color(0xff000000));
