a22b2c3049460beabd6c70e987da20cb32487d3b
whaeundo <1916388@sookmyung.ac.kr>
2023-02-04 15:39:47 +0900

    [Design/#21] Design mypage UI

--- lib/mypage/presentation/mypage_screen.dart
+++ lib/mypage/presentation/mypage_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:saera/mypage/presentation/widgets/mypage_background_image.dart';
+import 'package:saera/mypage/presentation/widgets/mypage_userInfo.dart';
+import 'package:saera/style/color.dart';
 import 'package:saera/style/font.dart';
 
 class MyPage extends StatefulWidget {
@@ -9,10 +13,84 @@ class MyPage extends StatefulWidget {
 }
 
 class _MyPageState extends State<MyPage> {
+
+  Widget _mypageButton(String label, String icon, bool isEnter) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 3),
+      child: ElevatedButton(
+          onPressed: null,
+          style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(ColorStyles.searchFillGray),
+              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0)
+                  )
+              )
+          ),
+          child: Container(
+            padding: const EdgeInsets.symmetric(vertical: 11),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SvgPicture.asset('assets/icons/${icon}'),
+                    Padding(padding: EdgeInsets.only(right: 9)),
+                    Text(
+                        label,
+                        style: TextStyles.medium00TextStyle
+                    ),
+                  ],
+                ),
+                    (){
+                  if(isEnter == true){
+                    return SvgPicture.asset('assets/icons/enter.svg');
+                  }
+                  else{
+                    return Spacer();
+                  }
+                }(),
+              ],
+            ),
+          )
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("마이페이지"),
+    return Stack(
+      children: [
+        const MyPageBackgroundImage(key: null,),
+        SafeArea(
+            child: Scaffold(
+                backgroundColor: Colors.transparent,
+                resizeToAvoidBottomInset: false,
+                body: Center(
+
+                    child: Container(
+                      margin: const EdgeInsets.only(left: 14, right: 14),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          UserInfo(),
+                          Container(
+                            margin: EdgeInsets.only(top: 53),
+                            child: _mypageButton("프로필 수정", "edit.svg", true),
+                          ),
+                          _mypageButton("로그아웃", "signout.svg", false)
+                        ],
+                      ),
+                    )
+                )
+            )
+        )
+
+
+      ],
     );
   }
 }
+
--- lib/mypage/presentation/widgets/mypage_background_image.dart
+++ lib/mypage/presentation/widgets/mypage_background_image.dart
@@ -1,17 +1,30 @@
 import 'package:flutter/material.dart';
 
-class AccentPracticeBackgroundImage extends StatelessWidget {
-  const AccentPracticeBackgroundImage({Key? key}) : super(key: key);
+class MyPageBackgroundImage extends StatelessWidget {
+  const MyPageBackgroundImage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.only(top:100.0),
       decoration: const BoxDecoration(
         image: DecorationImage(
           alignment: Alignment.topLeft,
           image: AssetImage('assets/images/top_bird.png'), // 배경 이미지
+            scale: 0.9
         ),
         color: Color(0xffFFFFFF),
+      ),
+      child: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              alignment: Alignment.topRight,
+              image: AssetImage('assets/images/mypage_flower.png'),
+              scale: 2
+            // 배경 이미지
+          ),
+          color: Color(0xffFFFFFF),
+        ),
       ),
     );
   }
