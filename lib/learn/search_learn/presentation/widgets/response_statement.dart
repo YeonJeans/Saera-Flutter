9d790231968a1d3b10f4e37fa6f84ce766759c5b
yeoncheong <yjin1100@sookmyung.ac.kr>
2023-02-04 14:21:52 +0900

    [Feature/#19] Connect search statement api

---
+++ lib/learn/search_learn/presentation/widgets/response_statement.dart
@@ -0,0 +1,21 @@
+class Statement {
+  final int id;
+  final String content;
+  final List<String> tags;
+  final bool bookmarked;
+
+  Statement(
+      {required this.id,
+        required this.content,
+        required this.tags,
+        required this.bookmarked});
+
+  factory Statement.fromJSON(Map<String, dynamic> json) {
+    return Statement(
+        id: json['id'],
+        content: json['content'],
+        tags: json['tags'],
+        bookmarked: json['bookmarked'],
+    );
+  }
+}
