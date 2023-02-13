5ddacbcf8ff7390e6c4fab59bedee4adf11ed87a
whaeundo <1916388@sookmyung.ac.kr>
2023-02-13 21:41:00 +0900

    [Design/#25] Design login screen

--- lib/login/presentation/login_screen.dart
+++ lib/login/presentation/login_screen.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:saera/style/font.dart';
 import 'package:saera/tabbar.dart';
+
+import '../../style/color.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -9,14 +14,96 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+
+  Widget googleLoginBtn (){
+    return GestureDetector(
+      onTap: () => Get.to(TabBarMainPage()),
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 8),
+        decoration: BoxDecoration(
+          color: ColorStyles.saeraWhite,
+          borderRadius: BorderRadius.circular(2), //border radius exactly to ClipRRect
+          boxShadow:[
+            BoxShadow(
+              color: const Color(0xff000000).withOpacity(0.3),
+              spreadRadius: 0.2,
+              blurRadius: 8,
+              offset: const Offset(2, 4), // changes position of shadow
+            ),
+          ],
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: const [
+            Image(
+              image: AssetImage('assets/icons/google_logo.png'),
+              width: 18,
+              height: 18,
+            ),
+            SizedBox(width: 24,),
+            Text(
+              "Google 계정으로 로그인",
+              style: TextStyles.medium25TextStyle,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget appleLoginBtn (){
+    return GestureDetector(
+      onTap: () => Get.to(TabBarMainPage()),
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 8),
+        decoration: BoxDecoration(
+          color: Colors.black,
+          borderRadius: BorderRadius.circular(2), //border radius exactly to ClipRRect
+          boxShadow:[
+            BoxShadow(
+              color: const Color(0xff663e68a8).withOpacity(0.3),
+              spreadRadius: 0.2,
+              blurRadius: 8,
+              offset: const Offset(0, 0), // changes position of shadow
+            ),
+          ],
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: const [
+            Image(
+              image: AssetImage('assets/icons/google_logo.png'),
+              width: 18,
+              height: 18,
+            ),
+            SizedBox(width: 24,),
+            Text(
+              "Apple로 로그인",
+              style: TextStyles.medium25TextStyle,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: ElevatedButton(
-        onPressed: () {
-          Navigator.push(context, MaterialPageRoute(builder: (context) => TabBarMainPage()));
-        },
-        child: Text("로그인"),
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/images/splash_bg.png'),
+          fit: BoxFit.fill
+        )
+      ),
+      padding: const EdgeInsets.only(left: 21, right: 21, bottom: 180),
+      child: Column(
+        children: [
+          const Spacer(),
+          googleLoginBtn(),
+          // appleLoginBtn()
+        ],
       )
     );
   }
