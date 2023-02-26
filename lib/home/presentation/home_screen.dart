90225abc7d330d03ae31bbcdf90fd19983caba24
yeoncheong <yjin1100@sookmyung.ac.kr>
2023-02-26 15:06:06 +0900

    [Design/#45] Design background setting

--- lib/home/presentation/home_screen.dart
+++ lib/home/presentation/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../style/font.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -8,9 +10,49 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String name = '수연';
   
   @override
   Widget build(BuildContext context) {
+
+    Widget greetingTextSection = Container(
+      margin: EdgeInsets.only(
+          top: MediaQuery.sizeOf(context).height*0.10,
+          left: MediaQuery.sizeOf(context).width*0.63
+      ),
+      child: Text(
+        '$name 님,\n 어서 오세요!',
+        style: TextStyles.xLarge25TextStyle,
+        textAlign: TextAlign.right,
+      ),
+    );
+
+    Widget studyTextSection = Container(
+      margin: EdgeInsets.only(
+          top: MediaQuery.sizeOf(context).height*0.185,
+          left: MediaQuery.sizeOf(context).width*0.60
+      ),
+      child: Text(
+        '오늘은 무엇을 학습할까요?',
+        style: TextStyles.small25TextStyle,
+        textAlign: TextAlign.right,
+      ),
+    );
+
+    Widget container = Container(
+      margin: EdgeInsets.only(top: MediaQuery.sizeOf(context).height*0.27),
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(topLeft: Radius.circular(32.0), topRight: Radius.circular(32.0)),
+        boxShadow: [
+          BoxShadow(
+            color: Color(0xffd9d9d9),
+            blurRadius: 30.0,
+            offset: Offset(0, -3)
+          )
+        ]
+      ),
+    );
 
     return Stack(
       children: [
@@ -18,7 +60,13 @@ class _HomePageState extends State<HomePage> {
             child: Scaffold(
               backgroundColor: Color(0xffBBE0CE),
               resizeToAvoidBottomInset: false,
-              body: Container(),
+              body: Stack(
+                children: [
+                  greetingTextSection,
+                  studyTextSection,
+                  container
+                ],
+              ),
             )
         )
       ],
