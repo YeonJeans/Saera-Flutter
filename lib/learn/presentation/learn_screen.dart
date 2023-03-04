3af5a703be4e52db4324767415e30a035ea3fb7f
yeoncheong <yjin1100@sookmyung.ac.kr>
2023-03-04 13:34:20 +0900

    [Design/#50] Design PreferredSize Appbar

--- lib/learn/presentation/learn_screen.dart
+++ lib/learn/presentation/learn_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:get/get.dart';
+import 'package:saera/style/color.dart';
+import 'package:saera/style/font.dart';
 
 class LearnPage extends StatefulWidget {
   const LearnPage({Key? key}) : super(key: key);
@@ -11,13 +15,54 @@ class _LearnPageState extends State<LearnPage> {
   @override
   Widget build(BuildContext context) {
 
+    Widget backSection = Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        TextButton.icon(
+            onPressed: () => Get.back(),
+            style: ElevatedButton.styleFrom(backgroundColor: Colors.transparent),
+            icon: SvgPicture.asset(
+              'assets/icons/back.svg',
+              fit: BoxFit.scaleDown,
+              color: ColorStyles.backIconGreen,
+            ),
+            label: const Text(' 뒤로',
+                style: TextStyles.backBtnTextStyle
+            )
+        ),
+      ],
+    );
+
+    Widget whatLearnSection = Container(
+      padding: EdgeInsets.only(top: MediaQuery.of(context).size.height*0.05, left: 10, right: 10),
+      child: const Text(
+        '무엇을 학습할까요?',
+        style: TextStyles.xLarge25TextStyle,
+      ),
+    );
+
+    Widget whatLearnTextSection = Container(
+      padding: EdgeInsets.only(top: MediaQuery.of(context).size.height*0.03, left: 10, right: 10),
+      child: const Text(
+        '발음 학습으로 올바른 남한 표준어 발음을,\n문장 학습으로 자주 사용하는 표현과 억양을 익힐 수 있어요.',
+        style: TextStyles.regular25TextStyle,
+      ),
+    );
+
     return SafeArea(
         child: Scaffold(
-            backgroundColor: Colors.transparent,
-            resizeToAvoidBottomInset: false,
             appBar: PreferredSize(
-              preferredSize: Size.fromHeight(230),
-              child: Container(),
+              preferredSize: Size.fromHeight(MediaQuery.of(context).size.height*0.24),
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                child: ListView(
+                  children: [
+                    backSection,
+                    whatLearnSection,
+                    whatLearnTextSection
+                  ],
+                ),
+              )
             ),
             body: Container(),
         )
