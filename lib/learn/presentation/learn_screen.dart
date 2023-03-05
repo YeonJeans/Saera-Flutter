ef7dfa02a7a4ac0ef26e08df9e51e33f40e2ca8b
yeoncheong <yjin1100@sookmyung.ac.kr>
2023-03-05 18:11:32 +0900

    [Design/#50] Redesign Learn Screen

--- lib/learn/presentation/learn_screen.dart
+++ lib/learn/presentation/learn_screen.dart
@@ -49,6 +49,103 @@ class _LearnPageState extends State<LearnPage> {
       ),
     );
 
+    Container buttonTitle(String text) {
+      return Container(
+        margin: EdgeInsets.only(top: MediaQuery.of(context).size.height*0.05, left: MediaQuery.of(context).size.width*0.07),
+        child: Text(
+          text,
+          style: TextStyles.large00MediumTextStyle,
+        ),
+      );
+    }
+    
+    Container buttonDescription(String text) {
+      return Container(
+        margin: EdgeInsets.only(top: MediaQuery.of(context).size.height*0.095, left: MediaQuery.of(context).size.width*0.07),
+        child: Text(
+          text,
+          style: TextStyles.regular55TextStyle,
+        ),
+      );
+    }
+
+    Widget goWordButtonSection = InkWell(
+      onTap: null,
+      child: Container(
+        padding: EdgeInsets.only(bottom: MediaQuery.of(context).size.height*0.035),
+        decoration: const BoxDecoration(
+          color: ColorStyles.saeraWord,
+          borderRadius: BorderRadius.all(Radius.circular(16.0))
+        ),
+        child: Stack(
+          children: [
+            buttonTitle('발음 학습'),
+            buttonDescription('단어를 따라 읽으며\n올바른 표준어 발음을 익혀요.'),
+            Container(
+              margin: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height*0.1,
+                  left: MediaQuery.of(context).size.width*0.574
+              ),
+              child: SvgPicture.asset('assets/icons/word_button_1.svg')
+            ),
+            Container(
+              margin: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height*0.18,
+                  left: MediaQuery.of(context).size.width*0.45
+              ),
+              child: SvgPicture.asset('assets/icons/word_button_2.svg'),
+            ),
+          ],
+        ),
+      ),
+    );
+
+    Widget goStatementButtonSection = InkWell(
+      onTap: null,
+      child: Container(
+        margin: EdgeInsets.only(top: MediaQuery.of(context).size.height*0.03),
+        padding: EdgeInsets.only(bottom: MediaQuery.of(context).size.height*0.035),
+        decoration: const BoxDecoration(
+            color: ColorStyles.saeraStatement,
+            borderRadius: BorderRadius.all(Radius.circular(16.0))
+        ),
+        child: Stack(
+          children: [
+            buttonTitle('억양 학습'),
+            buttonDescription('자연스러운 표준어 표현과\n억양을 익혀요.'),
+            Container(
+                margin: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height*0.1,
+                    left: MediaQuery.of(context).size.width*0.562
+                ),
+                child: SvgPicture.asset('assets/icons/statement_button_1.svg')
+            ),
+            Container(
+              margin: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height*0.18,
+                  left: MediaQuery.of(context).size.width*0.45
+              ),
+              child: SvgPicture.asset('assets/icons/statement_button_2.svg'),
+            ),
+            Container(
+                margin: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height*0.078,
+                    left: MediaQuery.of(context).size.width*0.61
+                ),
+                child: SvgPicture.asset('assets/icons/statement_button_1_1.svg')
+            ),
+            Container(
+                margin: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height*0.165,
+                    left: MediaQuery.of(context).size.width*0.49
+                ),
+                child: SvgPicture.asset('assets/icons/statement_button_2_1.svg')
+            ),
+          ],
+        ),
+      ),
+    );
+
     return SafeArea(
         child: Scaffold(
             appBar: PreferredSize(
@@ -56,6 +153,7 @@ class _LearnPageState extends State<LearnPage> {
               child: Container(
                 padding: const EdgeInsets.symmetric(horizontal: 10.0),
                 child: ListView(
+                  physics: const NeverScrollableScrollPhysics(),
                   children: [
                     backSection,
                     whatLearnSection,
@@ -64,7 +162,17 @@ class _LearnPageState extends State<LearnPage> {
                 ),
               )
             ),
-            body: Container(),
+            body: Container(
+              margin: EdgeInsets.only(top: MediaQuery.of(context).size.height*0.03),
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: ListView(
+                physics: const NeverScrollableScrollPhysics(),
+                children: [
+                  goWordButtonSection,
+                  goStatementButtonSection
+                ],
+              ),
+            ),
         )
     );
   }
