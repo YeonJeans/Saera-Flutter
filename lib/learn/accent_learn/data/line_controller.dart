167b0bed8c473509536f45bced4e010e1ff19e2f
whaeundo <1916388@sookmyung.ac.kr>
2023-03-15 20:41:54 +0900

    [Fix/#85] Add record value

--- lib/learn/accent_learn/data/line_controller.dart
+++ lib/learn/accent_learn/data/line_controller.dart
@@ -4,6 +4,9 @@ class LineController extends GetxController {
   Rx<double> duration = 2.0.obs;
   Rx<double> position = 0.0.obs;
 
+  Rx<double> rduration = 2.0.obs;
+  Rx<double> rposition = 0.0.obs;
+
   void setting(double dur) {
     duration.value = dur;
   }
@@ -18,5 +21,21 @@ class LineController extends GetxController {
 
   double getPosition(){
     return position.value;
+  }
+
+  void rsetting(double dur) {
+    rduration.value = dur;
+  }
+
+  void rpositionChanged(double pos){
+    rposition.value = pos;
+  }
+
+  double rgetDuration(){
+    return rduration.value;
+  }
+
+  double rgetPosition(){
+    return rposition.value;
   }
 }
