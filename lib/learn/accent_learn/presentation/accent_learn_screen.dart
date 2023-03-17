c48bccd512239f6a4c4816e1e87a41a4f8a6c64b
whaeundo <1916388@sookmyung.ac.kr>
2023-03-18 01:40:01 +0900

    [Chore/#105] Set statusbar color

--- lib/learn/accent_learn/presentation/accent_learn_screen.dart
+++ lib/learn/accent_learn/presentation/accent_learn_screen.dart
@@ -3,6 +3,7 @@ import 'dart:io' show Platform;
 
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_sound/public/flutter_sound_recorder.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
