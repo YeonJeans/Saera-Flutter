67ffc50debdcf3571f73ab82abf247d258218d8f
yeoncheong <yjin1100@sookmyung.ac.kr>
2023-03-22 23:26:14 +0900

    [Chore/#113] Modify Text

--- lib/home/bookmark_home/presentation/bookmark_home_screen.dart
+++ lib/home/bookmark_home/presentation/bookmark_home_screen.dart
@@ -141,7 +141,7 @@ class _BookmarkPageState extends State<BookmarkPage> {
     Widget textSection = Container(
       padding: const EdgeInsets.only(top: 50, left: 10, right: 10),
       child: Text(
-        _selectedIndex == 0 ? "${_authManager.getName()}님이 즐겨찾기한\n단어들이에요." : "${_authManager.getName()}님이 즐겨찾기한\n문장들이에요.",
+        _selectedIndex == 0 ? "${_authManager.getName()}님이 즐겨찾기한\n발음들이에요." : "${_authManager.getName()}님이 즐겨찾기한\n억양들이에요.",
         style: TextStyles.xxLargeTextStyle
       )
     );
